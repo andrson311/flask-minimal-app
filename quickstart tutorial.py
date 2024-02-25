3bebba3c909fe3f21c06e1e9a6a764f48aae68d3
Andrson311 <andraz.krzisnik1993@gmail.com>
2024-02-26 00:33:06 +0100

    simple blogging app

---
+++ quickstart tutorial.py
@@ -0,0 +1,86 @@
+from flask import Flask
+from flask import url_for, request, abort, redirect
+from markupsafe import escape
+
+app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#     return "<p>This is the index page!</p>"
+
+@app.route('/<name>')
+def hello(name):
+    return f"Hello, {escape(name)}!"
+
+@app.route('/hello')
+def hello_world():
+    return '<h1>Hello, world!</h1>'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'User {escape(username)}\'s profile'
+
+
+#Converter types: string, int, float, path, uuid
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f'Subpath {escape(subpath)}'
+
+# @app.route('/login')
+# def login():
+#     return 'login'
+
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('login'))
+#     print(url_for('login', next='/'))
+#     print(url_for('profile', username='John Doe'))
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
+
+# @app.get('/login')
+# def login_get():
+#     return show_the_login_form()
+
+# @app.post('/login')
+# def login_post():
+#     return do_the_login()
+
+from flask import session
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
